5463d4f0bda80cb9689ba7d2f661a50f75109951
covolan <covolan09@gmail.com>
2024-08-07 09:28:12 -0300

    changed colorscheme to catppuccin

--- lua/covolan/lazy/colorscheme.lua
+++ lua/covolan/lazy/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-	"folke/tokyonight.nvim",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
 	init = function()
-		vim.cmd.colorscheme("tokyonight-night")
-		vim.cmd.hi("Comment gui=none")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
